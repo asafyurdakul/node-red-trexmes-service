ca184632298adaacd7c43789c02f1f7cf5d14df0
Asaf Yurdakul <asafyurdakul@trex.com.tr>
2025-04-09 11:30:13 +0300

    ishandled default değeri false yapıldı

--- custom-form.js
+++ custom-form.js
@@ -87,6 +87,47 @@ module.exports = function(RED) {
         });
     });
 
+
+/*
+RED.httpAdmin.post("/custom-form/run-exe", function (req, res) {
+    const exePath = RED.settings.functionGlobalContext.customFormDesignerPath;
+    if (!exePath) {
+        res.status(500).send("Designer exe path not defined!");
+        return;
+    }
+
+    const xmlContent = req.body.xml;
+    const filename = req.body.filename;
+    const arg = `${filename}|${xmlContent}`;
+
+    // "start" komutu ile yeni shell'de çalıştır, GUI'yi gösterebilmek için
+    const command = `start "" "${exePath}" "${arg}"`;
+
+    const process = spawn("cmd", ["/c", command], {
+        detached: true,
+        stdio: "ignore", // process'e bağlı kalmamak için
+        windowsHide: false
+    });
+
+    // Süreci kapat ve cevap dön
+    process.unref(); // ana işlemden ayır
+
+    // XML çıktısını hemen değil, biraz gecikmeli olarak kontrol et
+    setTimeout(() => {
+        const outputPath = `c:/temp/${filename}_form_design.xml`;
+        if (!fs.existsSync(outputPath)) {
+            return res.status(404).json({ success: false, error: "File not found" });
+        }
+
+        fs.readFile(outputPath, "utf8", (err, data) => {
+            if (err) {
+                return res.status(500).json({ success: false, error: err.message });
+            }
+            res.json({ success: true, output: data });
+        });
+    }, 3000); // 3 saniye sonra dosya var mı kontrol et
+});
+*/
     RED.nodes.registerType("Custom Form",CustomForm);
 
 }
